bfed7e43d5a98860f5e36329bdd9d85d4c90b381
Erin Bleiweiss <erinbleiweiss@gmail.com>
2023-07-11 22:19:57 -0500

    Add test users to DB

--- backend/migrations/1689094800_schema.up.sql
+++ backend/migrations/1689094800_schema.up.sql
@@ -17,3 +17,33 @@ VALUES (
         '$2a$08$nkNm7OG2UrCbRK4yJElsKe4al9kSR8afMVoaPwV8G9klvuDZ3692i',
         'admin'
 );
+
+INSERT INTO user ( first_name, last_name, email, username, password, role)
+VALUES (
+           'Ron',
+           'Swanson',
+           'rswanson@pawnee.gov',
+           'rswanson',
+           '$2a$08$PnddSYjF.C98gBX5NeW1tOJHRMLcOO0jZBEY0HCwSJ7g5gmvNUJ.e',
+           'user'
+       );
+
+INSERT INTO user ( first_name, last_name, email, username, password, role)
+VALUES (
+           'Leslie',
+           'Knope',
+           'lknope@pawnee.gov',
+           'lknope',
+           '$2a$08$RfwujkXmh28BeR2JicQvpuU9ZCt9VgaMTVoWWnz4ek2dufHf//fei',
+           'user'
+       );
+
+INSERT INTO user ( first_name, last_name, email, username, password, role)
+VALUES (
+           'Ben',
+           'Wyatt',
+           'bwyatt@pawnee.gov',
+           'bwyatt',
+           '$2a$08$uyG.nsxf/ErmkUhRYSUvGOP3zAnR.bkVcTMdD3eKFZjYPvsi2RKve',
+           'user'
+       );
